8796d7c6716e749ff39fbfeb29efa6c0d45b2a37
vijay <macharlavijay6@gmail.com>
2019-05-28 09:23:23 +0530

    Aware Interface

---
+++ src/main/java/com/stackroute/awareInterface/ApplicationContextAwareDemo.java
@@ -0,0 +1,14 @@
+package com.stackroute.awareInterface;
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class ApplicationContextAwareDemo implements ApplicationContextAware {
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application context Aware : "+applicationContext);
+    }
+}
--- src/main/java/com/stackroute/config/SpringBeanConfiguration.java
+++ src/main/java/com/stackroute/config/SpringBeanConfiguration.java
@@ -2,6 +2,9 @@ package com.stackroute.config;
 
 import com.stackroute.Actor;
 import com.stackroute.Movie;
+import com.stackroute.awareInterface.ApplicationContextAwareDemo;
+import com.stackroute.awareInterface.BeanFactoryAwareDemo;
+import com.stackroute.awareInterface.BeanNameAwareDemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,5 +21,23 @@ public class SpringBeanConfiguration {
     public Movie getMovie(){
         return new Movie();
     }
+
+    /* Aware Interface */
+    @Bean(name="applicationContextAwareDemo")
+    public ApplicationContextAwareDemo getApplicationContextAwareDemo(){
+        return new  ApplicationContextAwareDemo();
+    }
+    @Bean(name="beanFactoryAwareDemo")
+    public BeanFactoryAwareDemo getBeanFactoryAwareDemo(){
+        return new BeanFactoryAwareDemo();
+    }
+    @Bean(name="beanNameAwareDemo")
+    public BeanNameAwareDemo getBeanNameAwareDemo(){
+        return new BeanNameAwareDemo();
+    }
+
+
+
+
 }
 
